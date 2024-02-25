c4822f3022f899aa5476bace791b9bb7da9864e7
Peter Kang <peterjkang@gmail.com>
2024-02-25 18:17:12 -0500

    Add variables

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is {x}");
+    let x = x + 1;
+    println!("The value of x is {x}");
+    {
+        let x = x * 2;
+        println!("The value of x is {x}");
+    }
+    println!("The value of x is {x}");
+}
