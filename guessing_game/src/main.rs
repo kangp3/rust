0ba70e6939f0fc8b5ceacfbec89791e9a35177be
Peter Kang <peterjkang@gmail.com>
2024-02-25 18:46:01 -0500

    Reimplement the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::io;
+use std::cmp;
+
 fn main() {
-    println!("Hello, world!");
+    let secret = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut buf = String::new();
+        println!("Input your guess");
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Failed to read line");
+
+        let input: u32 = match buf.trim().parse::<u32>() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            }
+        };
+
+        match input.cmp(&secret) {
+            cmp::Ordering::Less => println!("Too low"),
+            cmp::Ordering::Greater => println!("Too high"),
+            cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
