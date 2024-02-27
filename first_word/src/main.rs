16f1cd45687b8a5db56d9a0a0a66a4d9b92d40a3
Peter Kang <peterjkang@gmail.com>
2024-02-27 18:54:31 -0500

    Add first_word function

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello world");
+    let f = first_word(&s);
+    println!("First word is: {f}");
+}
+
+fn first_word(s: &str) -> &str {
+    let b = s.as_bytes();
+    for (i, &c) in b.iter().enumerate() {
+        if c == b' ' {
+            return &s[..i];
+        }
+    }
+    s
 }
