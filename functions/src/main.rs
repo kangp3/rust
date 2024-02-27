cda6f2099d00c6ce101a41e5fd4e092dcf9f0a20
Peter Kang <peterjkang@gmail.com>
2024-02-26 19:42:05 -0500

    Adjust another_function to take a parameter

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn print_labeled_measurement(x: i32, label: char) {
+    println!("The measurement is: {x}{label}");
 }
