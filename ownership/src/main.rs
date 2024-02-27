f9cdb37259009f9ca1f951aaa1a5f1853812e145
Peter Kang <peterjkang@gmail.com>
2024-02-27 17:03:22 -0500

    Bad borrow from function call

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,4 +3,18 @@ fn main() {
     let s2 = s1.clone();
 
     println!("s1: {s1}, s2: {s2}");
+
+    let s = String::from("hello");
+    let x = 5;
+    takes_ownership(s);
+    makes_copy(x);
+    println!("s: {s}, x: {x}");
+}
+
+fn takes_ownership(s: String) {
+    println!("{s}");
+}
+
+fn makes_copy(x: i64) {
+    println!("{x}");
 }
