9d8e539eb014814eb9549861d16239f425799457
Peter Kang <peterjkang@gmail.com>
2024-02-26 20:30:36 -0500

    Add break with loop label

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -10,4 +10,25 @@ fn main() {
     };
 
     println!("the result is {result}");
+
+    let mut count = 0;
+    let result = 'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up "asdf";
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    };
+    println!("End count = {count}");
+    println!("result = {result}");
 }
