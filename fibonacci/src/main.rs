f12d21b5624fa2890eb53ca8209eb500f6d5ae70
Peter Kang <peterjkang@gmail.com>
2024-02-26 22:46:13 -0500

    Implement non-recursive

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,12 +1,28 @@
 fn main() {
-    let n = 6;
+    let n = 41;
     let n_fib = fibonacci(n);
+    println!("{n} Fibonacci number is: {n_fib}");
+    let n_fib = fibonacci_recursive(n);
     println!("{n} Fibonacci number is: {n_fib}");
 }
 
-fn fibonacci(n: i32) -> i32 {
+fn fibonacci(n: u64) -> u64 {
     if n == 1 || n == 2 {
         return 1;
     }
-    fibonacci(n-1) + fibonacci(n-2)
+    let mut prev: u64 = 1;
+    let mut result: u64 = 1;
+    for _ in 0..n-2 {
+        let tmp = result;
+        result = result + prev;
+        prev = tmp;
+    }
+    return result;
+}
+
+fn fibonacci_recursive(n: u64) -> u64 {
+    if n == 1 || n == 2 {
+        return 1;
+    }
+    fibonacci_recursive(n-2) + fibonacci_recursive(n-1)
 }
