35fe06263c52222a2b9b8bf77fd030d5c821a65d
Peter Kang <peterjkang@gmail.com>
2024-02-26 20:07:20 -0500

    Add else-if

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     //let number = 3;
-    let number = 8;
+    let number = 5;
     
     if number < 5 {
         println!("condition was true");
     } else {
         println!("condition was false");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
