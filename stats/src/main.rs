ee55c4ac27edf0b604fa120526ea94c281054120
Peter Kang <peterjkang@gmail.com>
2024-03-01 14:41:47 -0500

    Compute mean, median, mode of dataset

--- stats/src/main.rs
+++ stats/src/main.rs
@@ -1,3 +1,50 @@
+use rand::Rng;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let mut v = vec!();
+    for _ in 0..100 {
+        v.push(rng.gen_range(1..=10));
+    }
+    v.sort();
+
+    println!("List is {:?}", &v);
+    println!("Mean is: {}", mean(&v));
+    println!("Median is: {}", median(&v));
+    println!("Mode is: {}", mode(&v));
+}
+
+fn mean(v: &Vec<i32>) -> f32 {
+    let mut sum = 0;
+    for i in v {
+        sum += i;
+    }
+    sum as f32 / v.len() as f32
+}
+
+fn median(v: &Vec<i32>) -> f32 {
+    let len = v.len();
+    if len % 2 == 1 {
+        return v[len/2] as f32;
+    }
+    (v[len/2] + v[len/2 + 1]) as f32 / 2 as f32
+}
+
+fn mode(v: &Vec<i32>) -> i32 {
+    let mut hm = HashMap::new();
+    for i in v {
+        let count = hm.entry(i).or_insert(0);
+        *count += 1;
+    }
+    let mut mode = 0;
+    let mut max_count = 0;
+    for (k, count) in hm {
+        if count > max_count {
+            mode = *k;
+            max_count = count;
+        }
+    }
+    mode
 }
