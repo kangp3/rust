159ebab17539d7c66c80cf3dfc7237426db9aa58
Peter Kang <peterjkang@gmail.com>
2024-03-02 18:57:27 -0500

    Finish pig latinizer

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -1,3 +1,20 @@
+const VOWELS: &[char] = &['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
 fn main() {
-    println!("Hello, world!");
+    let start_str = "Hello and end each world";
+    println!("Translating '{start_str}' to pig latin:\n -> {}", pig_latin(start_str));
+}
+
+fn pig_latin(s: &str) -> String {
+    let mut pig_latinized = String::new();
+    for word in s.split_whitespace() {
+        if word.starts_with(VOWELS) {
+            pig_latinized.push_str(&format!(" {word}-hay"));
+        } else {
+            let mut chars = word.chars();
+            let first = chars.next().expect("word should have first character");
+            pig_latinized.push_str(&format!(" {}-{first}ay", chars.collect::<String>()));
+        }
+    }
+    pig_latinized
 }
