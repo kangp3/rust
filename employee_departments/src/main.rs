1c13cab7966da4f80fc48d4a2bfd34fc9a7ea261
Peter Kang <peterjkang@gmail.com>
2024-03-02 22:51:53 -0500

    Checkpoint employee directory
    - Need to figure out how to allow words to survive between loop iters
    - Improve safety of Vec accesses?

--- employee_departments/src/main.rs
+++ employee_departments/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+use std::io::Write;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut directory: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("stdout should flush");
+
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).expect("stdin should be read");
+
+        let words: Vec<&str> = buf.split_whitespace().collect();
+        match words.get(0).expect("input should have command").to_lowercase().as_ref() {
+            "add" => {
+                if words.len() != 4 || words[2] != "to" {
+                    println!("Add command malformed (ex: 'Add Sally to Engineering')");
+                    continue;
+                }
+                let name = String::from(words[1]);
+                let department = String::from(words[3]);
+                directory.entry(&department).and_modify(|employees| { employees.push(&name) }).or_insert(vec!());
+                println!("Add {} to {}!", &name, &department);
+            }
+            "list" => println!("List!"),
+            cmd => println!("Command '{cmd}' not recognized"),
+        }
+    }
 }
