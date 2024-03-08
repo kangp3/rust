2dc3b2424805e74ce4391b4bc4fd1b228f76de0b
Peter Kang <peterjkang@gmail.com>
2024-03-07 20:21:23 -0500

    Get the sumbatch workin

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -10,37 +11,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config{
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-	let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
 
     Ok(())
 }
