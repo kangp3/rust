0c67dbbacbaf262641fbbef08b4ef75f23dfc0fd
Peter Kang <peterjkang@gmail.com>
2024-03-07 19:36:48 -0500

    Refactor into separate functions

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,46 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error processing arguments: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 
-	let contents = fs::read_to_string(file_path)
-        .expect("should be able to read file");
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config{
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+	let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+
+    Ok(())
 }
