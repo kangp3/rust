3c75fe3139a15f891300a2462fef59efc6af0e30
Peter Kang <peterjkang@gmail.com>
2024-03-07 22:20:21 -0500

    Print errors to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,12 @@ use minigrep::Config;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error processing arguments: {err}");
+        eprintln!("Error processing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 
