a61117ad6595eb5d34f1ad0c6bfd81d544836d89
Peter Kang <peterjkang@gmail.com>
2024-03-12 12:49:54 -0400

    Channel communication between threads

---
+++ channels/src/main.rs
@@ -0,0 +1,25 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    for received in rx {
+        println!("Received val {received}");
+    }
+}
