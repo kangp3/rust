c19957cb00cf805ae48e3508caf6d04588516bbb
Peter Kang <peterjkang@gmail.com>
2024-03-12 00:11:16 -0400

    Use Rcs for cons list impl

--- cons_list/src/main.rs
+++ cons_list/src/main.rs
@@ -1,12 +1,23 @@
 #[derive(Debug)]
 enum List<T> {
-    Cons(T, Box<List<T>>),
+    Cons(T, Rc<List<T>>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let list = Cons(5, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(2, Box::new(Nil))))))));
-    println!("List is: {:?}", list);
+    let a = Rc::new(Cons(3, Rc::new(Cons(4, Rc::new(Cons(2, Rc::new(Nil)))))));
+    println!("Ref count on creation is: {}", Rc::strong_count(&a));
+    let b = Cons(5, Rc::clone(&a));
+    println!("Ref count after constructing list b is: {}", Rc::strong_count(&a));
+    {
+        let c = Cons(6, Rc::clone(&a));
+        println!("Ref count after constructing list c is: {}", Rc::strong_count(&a));
+    }
+    println!("Ref count after dropping list c is: {}", Rc::strong_count(&a));
+    println!("List a is: {:?}", a);
+    println!("List b is: {:?}", b);
+    // println!("List c is: {:?}", c);
 }
