8a32f92636d370e9c30ab7b2f9c5c413d033d1f7
Peter Kang <peterjkang@gmail.com>
2024-03-12 12:45:32 -0400

    Send int message on channel

--- threaded_counts/src/main.rs
+++ threaded_counts/src/main.rs
@@ -2,15 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 0..10 {
-            println!("Counting {i}");
+            println!("Counting {i} from spawned thread");
             thread::sleep(Duration::from_millis(1));
         }
     });
 
     for i in 0..5 {
-        println!("Counting {i}");
+        println!("Counting {i} from main thread");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
